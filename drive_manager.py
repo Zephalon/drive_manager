b45607d705cb0d53106985d3774df134a95df57e
Fabian <fabian@prinz-arnold.de>
2022-05-18 21:00:33 +0200

    Added option to set maximum file age

--- drive_manager.py
+++ drive_manager.py
@@ -12,6 +12,7 @@ def bytesTo(bytes, to, bsize=1024):
 drive_path = '/media/nas'
 dir_path = drive_path + '/video'
 required_disk_space = 500 # in GiB
+maximum_file_age = 90 # in days, set to 'False' to disable it
 
 total, used, free = shutil.disk_usage(drive_path)
 
@@ -23,34 +24,43 @@ print("Required: %d GiB" % required_disk_space)
 free_space = free // (2**30)
 space_to_freed =  required_disk_space - free_space
 
+# lists to store files
+files = {}
+files_to_delete = []
+freed_space = 0
+
+# iterate directory
+for path in os.listdir(dir_path):
+    # check if current path is a file
+    if os.path.isfile(os.path.join(dir_path, path)):
+        files[path] = fileAgeInDays(dir_path + '/' + path)
+
+files = dict(sorted(files.items(), key=lambda item: item[1], reverse=True)) # sort files by age
+
+# free drive space
 if (space_to_freed > 0):
     print("Disk Space To Be Freed: %d GiB" % space_to_freed)
 
-    # lists to store files
-    files = {}
-    files_to_delete = []
-
-    # iterate directory
-    for path in os.listdir(dir_path):
-        # check if current path is a file
-        if os.path.isfile(os.path.join(dir_path, path)):
-            files[path] = fileAgeInDays(dir_path + '/' + path)
-
-    files = dict(sorted(files.items(), key=lambda item: item[1], reverse=True)) # sort files by age
-
     # mark files for deletion until enough space is freed
-    for file in files:
-        if(space_to_freed > 0):
+    while(space_to_freed > 0):
+        for file in sorted_files:
             space_to_freed -= bytesTo(os.stat(dir_path + '/' + path).st_size, 'g')
             files_to_delete.append(file)
-
-    print("Files To Be Deleted:")
-    print(files_to_delete)
-
-    # entering the danger zone
-    for file in files_to_delete:
-        os.remove(dir_path + '/' + file)
-    
-    print("Files Deleted!")
 else:
     print("Disk Space Left: %d GiB" % abs(space_to_freed))
+
+# delete old files
+if (maximum_file_age != False):
+    for file in files:
+        if (files[file] >= maximum_file_age):
+            files_to_delete.append(file)
+
+print("Files To Be Deleted:")
+print(files_to_delete)
+
+# entering the danger zone
+for file_path in files_to_delete:
+    os.remove(dir_path + '/' + file_path)
+    freed_space += bytesTo(os.stat(dir_path + '/' + file_path).st_size, 'g')
+
+print("Files Deleted, Space Freed: %d GiB" % freed_space)
